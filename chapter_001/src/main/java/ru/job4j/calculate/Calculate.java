b537a9ce8fe1ee1d0569fbfee404fec9c6bf0f6b
Sergey Malinkin <sloyz@yandex.ru>
2019-06-25 13:47:47 +0300

    1.3. JavaDoc. Code convention[#153302]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,19 @@
  package ru.job4j.calculate;
  
+ /**
+ * Calculate
+ * @author Sergey Malinkin (sloyz@ya.ru)
+ * @since 25.06.2019
+ * @version 1.0
+ */
+ 
  public class Calculate {
+	 
+ /**
+ * Метод Main
+ * Вывод надписи в консоль
+ * @param args - args
+ * /
  
 	public static void main (String [] args) {
 		System.out.println ("Hello World");
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Sergey Malinkin (sloyz@ya.ru)
+* @version 1.0
+* @since 25.06.2019
+*/
+package ru.job4j.calculate;
