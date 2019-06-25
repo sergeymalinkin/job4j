145073fa0899e39d7428ffb24ef4f4f3b5cd5bd2
Sergey Malinkin <sloyz@yandex.ru>
2019-06-25 14:48:26 +0300

    1.4. JUnit[#153303]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -9,14 +9,23 @@
  
  public class Calculate {
 	 
- /**
- * Метод Main
- * Вывод надписи в консоль
- * @param args - args
- */
+	/**
+	* Метод Main
+	* Вывод надписи в консоль
+	* @param args - args
+	*/
  
 	public static void main (String [] args) {
 		System.out.println ("Hello World");
 	}
 	
+	/**
+	* Method echo.
+	* @param name Your name.
+    * @return Echo plus your name.
+	*/
+	
+	public String echo(String name) {
+    return "Echo, echo, echo : " + name;
+	}
  }
