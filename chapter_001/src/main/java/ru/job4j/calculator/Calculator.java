21476fe99e23ed27da3f27e46a96d33655fb15da
Sergey Malinkin <sloyz@yandex.ru>
2019-06-27 13:01:45 +0300

    3.1. Элементарный калькулятор.[#153292]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,19 +1,42 @@
 package ru.job4j.calculator;
+/**
+ * Элементарный калькулятор.
+ * @author Sergey Malinkin (sloyz@ya.ru).
+ * @since 26.06.2019.
+ */
 
 public class Calculator {
+
+    /**
+     * Сложение.
+     * @return name - decription: add.
+     */
 
     public double add(double first, double second) {
         return first + second;
     }
 
+    /**
+     * Вычитание.
+     * @return name - decription: subtract.
+     */
+
     public double subtract(double first, double second) {
         return first - second;
     }
 
+    /**
+     * Деление.
+     * @return name - decription: div.
+     */
     public double div(double first, double second) {
         return first / second;
     }
 
+    /**
+     * Умножение.
+     * @return name - decription: multiple.
+     */
     public double multiple(double first, double second) {
         return first * second;
     }
