3686bd7050105fd909b2d55668d9128821a4df20
Sergey Malinkin <sloyz@yandex.ru>
2019-06-30 00:34:58 +0300

    4.4. Максимум из трех чисел[#153262]

--- chapter_001/src/test/java/ru/job4j/condition/MultiMaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MultiMaxTest.java
@@ -14,19 +14,19 @@ public class MultiMaxTest {
     @Test
     public void whenFirstMax() {
         MultiMax check = new MultiMax();
-        int result = check.max(1, 4, 2);
+        int result = check.max(4, 1, 2);
         assertThat(result, is(4));
     }
     @Test
     public void whenThirdMax() {
         MultiMax check = new MultiMax();
-        int result = check.max(1, 4, 2);
+        int result = check.max(1, 2, 4);
         assertThat(result, is(4));
     }
     @Test
     public void whenAllMax() {
         MultiMax check = new MultiMax();
-        int result = check.max(1, 4, 2);
+        int result = check.max(4, 4, 4);
         assertThat(result, is(4));
     }
 }
