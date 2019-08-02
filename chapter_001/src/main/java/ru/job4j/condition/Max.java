9b20cef573eaf810cd22a3cf8022cf2c649e4445
Sergey Malinkin <sloyz@yandex.ru>
2019-08-02 10:56:59 +0300

    Перегрузить метод max для трех чисел[#153372]

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -3,18 +3,22 @@
 /**
  * Перегруженный метод max должен возвращать максимальное число для трех и четырех чисел.
  * @author Sergey Malinkin(sloyz@ya.ru).
- * @version 1.0.
- * @since 01.08.2019
+ * @version 3.0.
+ * @since 02.08.2019
  */
 
 public class Max {
     public int max(int first, int second) {
-        return first > second? first : second;
+        return first > second ? first : second;
     }
-
+    public int max(int first, int second, int third) {
+        int tmp = max(first, second);
+        return max(third, tmp);
+    }
     public int max(int first, int second, int third, int fourth) {
-        int tmp = max(second, third);
-        return max(first,second)< tmp ? third : fourth;
+        int tmp = max(third, fourth);
+        int tmp2 = max(first, second);
+        return max(tmp, tmp2);
     }
 }
 
--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -25,16 +25,39 @@ public class MaxTest {
         int result = max.max(2, 2);
         assertThat(result, is(2));
     }
-    @Test
-    public void whenThirdMax() {
+    public void whenThirdMaxOfTheFour() {
         Max check = new Max();
         int result = check.max(1, 2, 4, 3);
         assertThat(result, Matchers.is(4));
     }
     @Test
-    public void whenFourthMax() {
+    public void whenFirstMaxOfTheFour() {
+        Max check = new Max();
+        int result = check.max(4, 3, 2, 3);
+        assertThat(result, Matchers.is(4));
+    }
+    @Test
+    public void whenFourthMaxOfTheFour() {
         Max check = new Max();
         int result = check.max(1, 3, 2, 4);
         assertThat(result, Matchers.is(4));
+    }
+    @Test
+    public void whenThirdMaxOfTheThree() {
+        Max check = new Max();
+        int result = check.max(1, 2, 3);
+        assertThat(result, Matchers.is(3));
+    }
+    @Test
+    public void whenSecondMaxOfTheThree() {
+        Max check = new Max();
+        int result = check.max(1, 3, 2);
+        assertThat(result, Matchers.is(3));
+    }
+    @Test
+    public void whenFirstMaxOfTheThree() {
+        Max check = new Max();
+        int result = check.max(3, 1, 2);
+        assertThat(result, Matchers.is(3));
     }
 }
