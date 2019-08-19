1e53a238d24fc778b25513bf4d46d4584d0a7bf1
Sergey Malinkin <sloyz@yandex.ru>
2019-08-19 16:00:13 +0300

    Исправлены ошибки после валидации Maven

--- puzzle/src/main/java/ru/job4j/puzzle/firuges/Cell.java
+++ puzzle/src/main/java/ru/job4j/puzzle/firuges/Cell.java
@@ -13,11 +13,16 @@ public class Cell {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Cell cell = (Cell) o;
-        return x == cell.x &&
-                y == cell.y;
+        return x == cell.x
+               &&
+               y == cell.y;
     }
 
     @Override
