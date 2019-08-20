39b3f0254481f6f53534925b33d78fed842698ce
Sergey Malinkin <sloyz@yandex.ru>
2019-08-20 18:20:48 +0300

    Расстояние между точками в трехмерном пространстве(объединение с веткой master)

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,10 +1,7 @@
 package ru.job4j.condition;
-
 import static java.lang.Math.sqrt;
 import static java.lang.Math.pow;
-
 public class Point {
-
     /**
      * Это поле объекта. Оно доступно только конкретному объекту.
      */
@@ -14,7 +11,7 @@ public class Point {
      * И это поле объекта. Оно доступно только конкретному объекту.
      */
     private int y;
-
+    private int z;
     /**
      * Конструтор, который принимает начальное состояние объекта "точка"
      * @param first координата x
@@ -24,15 +21,20 @@ public class Point {
         this.x = first;
         this.y = second;
     }
-
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
     public double distance(Point that) {
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
-
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
+    }
     public void info() {
         System.out.println(String.format("Point[%s, %s]", this.x, this.y));
     }
-
     public double distance(int x1, int y1, int x2, int y2) {
         return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
     }
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -1,6 +1,8 @@
 package ru.job4j.condition;
 
 import org.junit.Test;
+
+import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -15,14 +17,12 @@ public class PointTest {
         System.out.println(String.format("Result is %s", result));
         assertThat(result, is(10D));
     }
-
     @Test
     public void whenCheckItself() {
         Point point = new Point(0, 0);
         double result = point.distance(point);
         assertThat(result, is(0D));
     }
-
     @Test
     public void whenShowInfo() {
         Point first = new Point(1, 1);
@@ -30,4 +30,22 @@ public class PointTest {
         Point second = new Point(2, 2);
         second.info();
     }
+    @Test
+    public void whenTenAndTenThenEight3d() {
+        Point first = new Point(0, 0, 0);
+        Point second = new Point(10, 10, 8);
+        first.info();
+        second.info();
+        double result = first.distance3d(second);
+        System.out.println(String.format("Result is %s", result));
+        assertThat(result, closeTo(16.25, 0.1));
+    }
+    @Test
+    public void whenCheckItself3D() {
+        Point point = new Point(0, 0, 0);
+        double result = point.distance(point);
+        assertThat(result, is(0D));
+    }
+
+
 }
