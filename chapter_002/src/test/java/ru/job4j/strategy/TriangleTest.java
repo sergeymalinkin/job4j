bcedb311b7b309ec2a463f53c7bbe60cd130686a
Sergey Malinkin <sloyz@ya.ru>
2019-08-21 22:08:43 +0300

    Рефакторинг теста

--- chapter_002/src/test/java/ru/job4j/strategy/TriangleTest.java
+++ chapter_002/src/test/java/ru/job4j/strategy/TriangleTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertThat;
 /**
  * Created by Sergey Malinkin (sloyz@ya.ru) on 21.08.2019.
  */
-public class TriangleTest{
+public class TriangleTest {
     @Test
     public void whenDrawTriangle() {
         Triangle triangle = new Triangle();
