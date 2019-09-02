df978faf5d06470005abd864c4a56f4498277b3a
Sergey Malinkin <sloyz@yandex.ru>
2019-09-02 11:55:12 +0300

    Телефонный справочник на базе ArrayList - решение

--- chapter_003/src/main/java/ru/job/search/PhoneDictionary.java
+++ chapter_003/src/main/java/ru/job/search/PhoneDictionary.java
@@ -2,7 +2,11 @@ package ru.job.search;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * @author Sergey Malinkin (sloyz@ya.ru)
+ * @version 1.0
+ * @since 02.09.2019
+ */
 class PhoneDictionary {
     private List<Person> persons = new ArrayList<>();
 
@@ -16,7 +20,16 @@ class PhoneDictionary {
      */
     List<Person> find(String key) {
         List<Person> result = new ArrayList<>();
+
+        for (Person person : persons) {
+            if ((key.contains(person.getName())
+                    || key.contains(person.getSurname())
+                    || key.contains(person.getPhone())
+                    || key.contains(person.getAddress()))
+            ) {
+                result.add(person);
+            }
+        }
         return result;
     }
 }
-
--- chapter_003/src/test/java/ru/job/search/PhoneDictionaryTest.java
+++ chapter_003/src/test/java/ru/job/search/PhoneDictionaryTest.java
@@ -1,13 +1,22 @@
 package ru.job.search;
 
+import org.junit.Test;
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * @author Sergey Malinkin (sloyz@ya.ru)
+ * @version 1.0
+ * @since 02.09.2019
+ */
 public class PhoneDictionaryTest {
-//    @Test
-//    public void whenFindByName() {
-//        PhoneDictionary phones = new PhoneDictionary();
-//        phones.add(
-//                new Person("Petr", "Arsentev", "534872", "Bryansk")
-//        );
-//        List<Person> persons = phones.find("Petr");
-//        assertThat(persons.iterator().next().getSurname(), is("Arsentev"));
-//    }
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        List<Person> persons = phones.find("Petr");
+        assertThat(persons.iterator().next().getSurname(), is("Arsentev"));
+    }
 }
