823e8a642b3eb0ef7b4fc6a19d0c940e11420864
Sergey Malinkin <sloyz@yandex.ru>
2019-09-03 15:22:02 +0300

    Конвертация ArrayList в двухмерный массив

---
+++ chapter_003/src/main/java/ru/job/list/ConvertList2Array.java
@@ -0,0 +1,31 @@
+package ru.job.list;
+
+import java.util.List;
+/**
+ * @author Sergey Malinkin (sloyz@ya.ru)
+ * @version 1.0
+ * @since 03.09.2019
+ */
+class ConvertList2Array {
+    /**
+     * Метод конвертирует ArrayList в двухмерный массив.
+     *
+     */
+    int[][] toArray(List<Integer> list, int rows) {
+        int cells = (int) Math.ceil((double) list.size() / (double) rows);
+        int[][] array = new int[rows][cells];
+        int row = 0;
+        int cell = 0;
+        for (Integer l : list) {
+            array[row][cell++] = l;
+            if (cell >= cells) {
+                cell = 0;
+                row++;
+            }
+
+        }
+        return array;
+    }
+}
+
+
---
+++ chapter_003/src/test/java/ru/job/list/ConvertList2ArrayTest.java
@@ -0,0 +1,79 @@
+package ru.job.list;
+
+import org.junit.Test;
+import java.util.Arrays;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+/**
+ * @author Sergey Malinkin (sloyz@ya.ru)
+ * @version 1.0
+ * @since 03.09.2019
+ */
+public class ConvertList2ArrayTest {
+    /**
+     * Тест проверяет комбинацию преобразования 7 элементов списка в массив из 3-х строк
+     */
+    @Test
+    public void when7ElementsThen9Rows3() {
+        ConvertList2Array list = new ConvertList2Array();
+        int[][] result = list.toArray(Arrays.asList(1, 2, 3, 4, 5, 6, 7),3);
+        int[][] expect = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 0, 0}
+        };
+        assertThat(result, is(expect));
+    }
+    /**
+     * Тест проверяет комбинацию преобразования 7 элементов списка в массив из 4-х строк
+     */
+    @Test
+    public void when7ElementsThen8Rows4() {
+        ConvertList2Array list = new ConvertList2Array();
+        int[][] result = list.toArray(Arrays.asList(1, 2, 3, 4, 5, 6, 7), 4);
+        int[][] expect = {
+                {1, 2},
+                {3, 4},
+                {5, 6},
+                {7, 0}
+        };
+        assertThat(result, is(expect));
+    }
+    /**
+     * Тест проверяет комбинацию преобразования 7 элементов списка в массив из 5-х строк
+     */
+    @Test
+    public void when7ElementsThen10Rows5() {
+        ConvertList2Array list = new ConvertList2Array();
+        int[][] result = list.toArray(Arrays.asList(1, 2, 3, 4, 5, 6, 7), 5);
+        int[][] expect = {
+                {1, 2},
+                {3, 4},
+                {5, 6},
+                {7, 0},
+                {0, 0}
+        };
+        assertThat(result, is(expect));
+    }
+    /**
+     * Тест проверяет комбинацию преобразования 7 элементов списка в массив из 6-х строк
+     */
+    @Test
+    public void when7ElementsThen12Rows6() {
+        ConvertList2Array list = new ConvertList2Array();
+        int[][] result = list.toArray(Arrays.asList(1, 2, 3, 4, 5, 6, 7), 6);
+        int[][] expect = {
+                {1, 2},
+                {3, 4},
+                {5, 6},
+                {7, 0},
+                {0, 0},
+                {0, 0}
+        };
+        assertThat(result, is(expect));
+    }
+}
+
+
+
+
