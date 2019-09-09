a643357d038b59fa6932e84ee90e443b58a75486
Sergey Malinkin <sloyz@yandex.ru>
2019-09-09 15:18:12 +0300

    Ссылки на методы

---
+++ chapter_004/src/main/java/lambda/MathUtil.java
@@ -0,0 +1,26 @@
+package lambda;
+
+/**
+ * @author Sergey Malinkin (sloyz@ya.ru)
+ * @version 1
+ * @since 09.09.2019
+ */
+public class MathUtil {
+    static double add(int left, int second) {
+        return left + second;
+    }
+
+    public static double div(int left, int second) {
+        return left / second;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        calc.multiple(
+                0, 10, 2,
+                MathUtil::add,
+                System.out::println
+        );
+    }
+}
+
---
+++ chapter_004/src/test/java/lambda/MathUtilTest.java
@@ -0,0 +1,28 @@
+package lambda;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+/**
+ * @author Sergey Malinkin (sloyz@ya.ru)
+ * @version 1
+ * @since 09.09.2019
+ */
+public class MathUtilTest {
+    @Test
+    public void whenAdd1Until3() {
+        Calculator calc = new Calculator();
+        List<Double> buffer = new ArrayList<>();
+        calc.multiple(
+                0, 3, 1,
+                MathUtil::add, //static call
+                buffer::add // non-static call
+        );
+        assertThat(buffer, is(Arrays.asList(1D, 2D, 3D)));
+    }
+}
+
