b4243d620f80463f90730c0ac7443dd6e1a7fcbb
Sergey Malinkin <sloyz@yandex.ru>
2019-09-15 13:34:58 +0300

    Поиск индекса в диапазоне

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -1,13 +1,21 @@
 package ru.job4j.array;
 
-public class FindLoop {
-    public int indexOf(int[] data, int el) {
-//    int index;
+class FindLoop {
+    static int indexOf(int[] data, int el) {
         int rst = -1;
         for (int index = 0; index != data.length; index++) {
             if (data[index] == el) {
                 rst = index;
                 break;
+            }
+        }
+        return rst;
+    }
+    static int indexOf(int[] data, int el, int start, int finish) {
+        int rst = -1;
+        for (int i = start + 1; i <= finish ; i++) {
+            if (data[i] == el) {
+                rst = i;
             }
         }
         return rst;
--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -5,13 +5,30 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class FindLoopTest {
-
+    @Test
+    public void whenArrayHasLength5Then0() {
+        int[] input = new int[] {5, 10, 3};
+        int value = 5;
+        int result = FindLoop.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenFind3() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
     @Test
     public void whenArrayHas5Then0() {
         FindLoop find = new FindLoop();
         int[] input = new int[] {5, 10, 3};
         int value = 5;
-        int result = find.indexOf(input, value);
+        int result = FindLoop.indexOf(input, value);
         int expect = 0;
         assertThat(result, is(expect));
     }
@@ -20,7 +37,7 @@ public class FindLoopTest {
         FindLoop find = new FindLoop();
         int[] input = new int[] {8, 11, 4, 25};
         int value = 8;
-        int result = find.indexOf(input, value);
+        int result = FindLoop.indexOf(input, value);
         int expect = 0;
         assertThat(result, is(expect));
     }
@@ -29,10 +46,8 @@ public class FindLoopTest {
         FindLoop find = new FindLoop();
         int[] input = new int[] {8, 11, 4, -25};
         int value = -25;
-        int result = find.indexOf(input, value);
+        int result = FindLoop.indexOf(input, value);
         int expect = 3;
         assertThat(result, is(expect));
-
-
     }
 }
