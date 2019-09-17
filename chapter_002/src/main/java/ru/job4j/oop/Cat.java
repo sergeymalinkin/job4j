bc46fba7e09d33f21299b6dcdb097102a8cf19a3
Sergey Malinkin <sloyz@yandex.ru>
2019-09-17 21:37:34 +0300

    Котики обормотики

---
+++ chapter_002/src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+/**
+ * Котики-обормотики
+ * @author Sergey Malinkin (sloyz@ya.ru)
+ * @version 1
+ * @since 17.09.2019
+ */
+public class Cat {
+    public static void main(String[] args) {
+        Cat peppy = new Cat();
+        Cat sparky = new Cat();
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+/**
+ * Собачки-жрачки
+ * @author Sergey Malinkin (sloyz@ya.ru)
+ * @version 1
+ * @since 17.09.2019
+ */
+public class Dog {
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
