e9ea3560e4b94e3904768148b137fba1ee400ddf
Sergey Malinkin <sloyz@yandex.ru>
2019-09-18 13:16:44 +0300

    Котенок гав и его котлета

--- chapter_002/src/main/java/ru/job4j/oop/Cat.java
+++ chapter_002/src/main/java/ru/job4j/oop/Cat.java
@@ -2,12 +2,32 @@ package ru.job4j.oop;
 /**
  * Котики-обормотики
  * @author Sergey Malinkin (sloyz@ya.ru)
- * @version 1
- * @since 17.09.2019
+ * @version 2
+ * @since 18.09.2019
  */
 public class Cat {
+    private String name;
+    private String food;
+
+    private void eat(String meat) {
+        this.food = meat;
+    }
+    private void giveNick(String nick) {
+        this.name = nick;
+    }
+    private void show() {
+        System.out.println("There are cat's nick : " + this.name);
+        System.out.println("There are cat's food : " + this.food);
+    }
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("fish");
+        black.show();
+
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("kotleta");
+        gav.show();
     }
 }
